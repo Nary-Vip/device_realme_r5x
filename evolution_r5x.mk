4e7dab1ec342077b61720614677f2fc12915b238
Nary-Vip <nary2vip@gmail.com>
2021-06-15 16:56:33 +0530

    Updated to June Redfin FP
    RQ3A.210605.005

--- evolution_r5x.mk
+++ evolution_r5x.mk
@@ -42,6 +42,7 @@ TARGET_VENDOR_DEVICE_NAME := r5x
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="r5x" \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210405.005 7181113 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210605.005 7349499 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+
